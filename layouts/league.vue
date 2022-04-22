543f7431a1f13b2328ec9f267582115659819686
Julian van Veen <julianvv2000@gmail.com>
2022-04-22 13:29:56 +0200

    Added icons/logos and updated index

---
+++ layouts/league.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <league-header />
+    <Nuxt class="body" />
+    <base-footer />
+  </div>
+</template>
+
+<script>
+import LeagueHeader from "../components/Header/LeagueHeader.vue";
+import BaseFooter from "../components/BaseFooter.vue";
+
+export default {
+  components: {
+    LeagueHeader,
+    BaseFooter,
+  },
+};
+</script>
+
+<style scoped>
+main {
+  margin: 0 auto;
+  padding: 0 1rem;
+  margin-top: 88px;
+  max-width: 1280px;
+}
+
+.body {
+  padding: 0px 32px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,78 @@
 <template>
   <main>
-    <h1>Hello World!</h1>
-    <h2>Lol</h2>
+    <h1 class="title">SELECT A GAME</h1>
+    <div class="game-selectors">
+      <nuxt-link to="/league">
+        <div class="selector league-selector">
+          <img
+            src="../static/lol-logo-rendered-hi-res.png"
+            height="auto"
+            width="220px"
+          />
+        </div>
+      </nuxt-link>
+      <nuxt-link to="/valorant">
+        <div class="selector valorant-selector">
+          <img
+            src="../static/valorant/Logotype/V_Logotype_Off-White.png"
+            height="auto"
+            width="220px"
+          />
+        </div>
+      </nuxt-link>
+    </div>
   </main>
 </template>
+
+<script>
+export default {
+  layout: "default",
+};
+</script>
+
+<style scoped>
+main {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+div.game-selectors {
+	display: flex;
+	flex-direction: column;
+}
+
+@media (min-width: 768px) {
+  div.game-selectors {
+		flex-direction: row;
+	}
+}
+
+div.selector {
+  cursor: pointer;
+  margin: 6px;
+  height: 200px;
+  display: flex;
+  border-width: 2px;
+  border-style: solid;
+}
+
+div.selector.league-selector {
+	border-color:#C19738;
+}
+
+div.selector.valorant-selector {
+	border-color:#FF4655;
+}
+
+div.selector img {
+  align-self: center;
+}
+
+h1.title {
+  font-family: "Inter", sans-serif;
+	margin: 2rem;
+}
+</style>
